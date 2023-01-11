fb1bcab242e7df109daac4823491d82f84a9c330
张为杰 <zhangweijie@cestc.cn>
2023-01-11 16:32:27 +0800

    change test

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ import { v4 } from 'uuid'
 
 const app = express()
 
-app.get('/api', (req, res) => {
+app.get('/apix', (req, res) => {
   const path = `/api/item/${v4()}`
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
 })
 
-app.get('/api/item/:slug', (req, res) => {
+app.get('/apix/item/:slug', (req, res) => {
   const { slug } = req.params
   res.end(`Item: ${slug}`)
 })

1d73488bb9028c94a1677d59aa5580ab63820a42
张为杰 <zhangweijie@cestc.cn>
2023-01-11 16:35:40 +0800

    app

---
+++ api-old/index.js
@@ -0,0 +1,19 @@
+
+import express from 'express'
+import { v4 } from 'uuid'
+
+const app = express()
+
+app.get('/apix', (req, res) => {
+  const path = `/api/item/${v4()}`
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+})
+
+app.get('/apix/item/:slug', (req, res) => {
+  const { slug } = req.params
+  res.end(`Item: ${slug}`)
+})
+
+export default app
